ead7e019fe4c2c3f518363a26a52ff0d3b908a92
boki <lsb530@naver.com>
2024-07-15 01:11:44 +0900

    spring boot admin starter server

---
+++ src/test/java/com/boki/bokispringbootadminserver/BokiSpringbootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.bokispringbootadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokiSpringbootAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
